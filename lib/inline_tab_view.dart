b6effe499b2550fd95ea475a95b777ea4283d7d1
NobodyForNothing <contact@derdilla.com>
2024-10-11 17:27:36 +0200

    Initial commit

---
+++ lib/inline_tab_view.dart
@@ -0,0 +1,79 @@
+library inline_tab_view;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class InlineTabBarView extends StatelessWidget {
+  const InlineTabBarView({super.key,
+    required this.tabs,
+    required this.controller,
+  });
+
+  final List<Widget> tabs;
+
+  final TabController controller;
+
+  @override
+  Widget build(BuildContext context) => GestureDetector(
+    onHorizontalDragStart: (details) {
+      controller.offset = 0.0;
+    },
+    onHorizontalDragUpdate: (details) {
+      // FIXME
+      controller.offset = details.delta.dx / 2;
+      print(controller.offset);
+    },
+    onHorizontalDragEnd: (details) {
+      if (details.localPosition.dx > 0) {
+        controller.offset = 1.0;
+        controller.index++;
+      } else {
+        controller.offset = -1.0;
+        controller.index--;
+      }
+    },
+    onHorizontalDragCancel: () {},
+    child: ListenableBuilder(
+      listenable: controller,
+      builder: (BuildContext context, Widget? child) {
+        print(controller.animation?.value);
+        return AnimatedBuilder(
+          animation: controller.animation!,
+          builder: (BuildContext context, Widget? child) => Transform.translate(
+            //duration: controller.animationDuration,
+            //translation: Offset(2 * (controller.offset - 1), 0),
+            offset: Offset(-MediaQuery.of(context).size.width * controller.animation!.value, 0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                for (final t in tabs)
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(child: t),
+                  ),
+                /*if (controller.index - 1 >= 0)
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(child: tabs[controller.index - 1]),
+                  ),
+                AnimatedSize(
+                  duration: controller.animationDuration,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(child: tabs[controller.index]),
+                  ),
+                ),
+                if (controller.index + 1 < controller.length)
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(child: tabs[controller.index + 1]),
+                  ),*/
+              ],
+            ),
+          ),
+        );
+      },
+    ),
+  );
+}
+//FIXME: readd animated size
